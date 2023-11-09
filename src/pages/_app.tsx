d1f925460825ea9b2c22dcbb46267a436a4fd3e7
Hien Phung <pkhien95@gmail.com>
2023-11-09 15:28:21 +0700

    feat: add multichain and multiwallets

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,12 @@ import { createWeb3Modal, defaultWagmiConfig } from "@web3modal/wagmi/react";
 import { WagmiConfig } from "wagmi";
 import type { AppProps } from "next/app";
 import { useEffect, useState } from "react";
-import { ronin } from "wagmi/chains";
+import { ronin, mainnet, bsc } from "wagmi/chains";
 
 const chains = [
   ronin,
+  mainnet,
+  bsc
 ];
 
 // 1. Get projectID at https://cloud.walletconnect.com
@@ -29,7 +31,11 @@ createWeb3Modal({
   chains,
   defaultChain: ronin,
   featuredWalletIds: ['541d5dcd4ede02f3afaf75bf8e3e4c4f1fb09edb5fa6c4377ebf31c2785d9adf'],
-  includeWalletIds: ['541d5dcd4ede02f3afaf75bf8e3e4c4f1fb09edb5fa6c4377ebf31c2785d9adf'],
+  includeWalletIds: [
+    '541d5dcd4ede02f3afaf75bf8e3e4c4f1fb09edb5fa6c4377ebf31c2785d9adf',
+    'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96',
+    '4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0'
+  ],
   chainImages: {
     2020: 'https://roninchain.com/static/images/logo-on-white.svg'
   }
